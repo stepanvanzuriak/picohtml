e4233d4057e0c297ddb4f3cde5b082979fd6ac1e
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-09-09 14:35:02 +0300

    Refactoring

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,4 @@
-export const htmlEscape = str =>
-  str
-    .replace(/&/g, "&amp;")
-    .replace(/>/g, "&gt;")
-    .replace(/</g, "&lt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;")
-    .replace(/`/g, "&#96;");
-
-export const loopDOMAttributes = (dom, callback) => {
+const loopDOMAttributes = (dom, callback) => {
   const attrs = dom.attributes;
 
   if (attrs && attrs.length) {
@@ -25,21 +16,28 @@ export const loopDOMAttributes = (dom, callback) => {
   }
 };
 
+export const htmlEscape = str =>
+  str
+    .replace(/&/g, "&amp;")
+    .replace(/>/g, "&gt;")
+    .replace(/</g, "&lt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;")
+    .replace(/`/g, "&#96;");
+
 export const htmlToElements = (html, events) => {
   const template = document.createElement("template");
-  html = html.trim(); // Never return a text node of whitespace as the result
-  template.innerHTML = html;
+  template.innerHTML = html.trim();
 
   const node = template.content.firstChild;
 
   loopDOMAttributes(node, (name, element) => {
-    let event = events.shift();
+    const event = events.shift();
 
-    if (typeof event !== "function") {
-      event = () => {};
-    }
-
-    element.addEventListener(name, event);
+    element.addEventListener(
+      name,
+      typeof event !== "function" ? () => {} : event
+    );
   });
 
   return node;
