274f98d0cda542c8e6243257fe93267b3257c1be
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-10-10 15:13:02 +0300

    Fix render temolate comparison

--- src/render/render.ts
+++ src/render/render.ts
@@ -6,17 +6,23 @@ const Parts = new WeakMap<Node, Template>();
 export const render = (template: Template, node: Node) => {
   const part = Parts.get(node);
 
-  if (part) {
-    if (!part.checkValues(template.values)) {
-      cleanNode(node);
-      node.appendChild(toDOM(part.result, part.events));
-    }
-  } else {
+  const addToPart = () => {
     const { result, events } = template.getResult();
     Parts.set(node, template);
 
     cleanNode(node);
 
     node.appendChild(toDOM(result, events));
+  };
+
+  if (part) {
+    if (!part.checkLiteral(template.lit)) {
+      addToPart();
+    } else if (!part.checkValues(template.values)) {
+      cleanNode(node);
+      node.appendChild(toDOM(part.result, part.events));
+    }
+  } else {
+    addToPart();
   }
 };
